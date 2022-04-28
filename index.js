7a8eb6fc6321cd7f1779badc62a3b079333f1577
Aaron Lally <aaronblally@gmail.com>
2022-04-28 13:48:10 -0400

    commit tree 78a0c71e5a6331aebbe6cdcb67bf1ad0512f7ef9

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const hq = 42;
+const feet = 264;
+
+const distanceFromHqInBlocks = (someValue) => {
+if (someValue > 42) {
+    return someValue - 42;
+}
+else if (someValue < 42) {
+    return 42 - someValue;
+}
+}
+
+const distanceFromHqInFeet = (someValue) => {
+    return distanceFromHqInBlocks(someValue) * feet;
+}
+
+const distanceTravelledInFeet = (num1, num2) => {
+    if (num1 > num2) {
+        return (num1 - num2) * 264;
+    }
+    else if (num1 < num2) {
+        return (num2 - num1) *264;
+    }
+}
+
+const calculatesFarePrice = (num1, num2) => {
+    if (distanceTravelledInFeet(num1, num2) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(num1, num2) > 400 && distanceTravelledInFeet(num1, num2) <= 2000) {
+        return (distanceTravelledInFeet(num1, num2) - 400) * 2 / 100
+    }
+    else if (distanceTravelledInFeet(num1, num2) > 2000 && distanceTravelledInFeet(num1, num2) <= 2500) {
+        return 25
+    }
+    else return "cannot travel that far"
+}
